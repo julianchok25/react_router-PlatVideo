12438b291e8c8cdf9aa481946bc18974298b8753
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-22 14:48:17 -0500

    [ADD]Change home name to videos and create a new component for home with basic elements, finally, add two Routes with path and reference to components

--- src/entries/app.js
+++ src/entries/app.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { render } from "react-dom";
-import Home from "../pages/containers/home";
+import Videos from "../pages/containers/videos";
+import Home from "../pages/components/home";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "../reducers/index";
@@ -8,8 +9,10 @@ import { Map as map } from "immutable";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { BrowserRouter } from "react-router-dom";
-import Header from "../pages/components/headers";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Header from "../pages/components/headers.js";
 // function logger({ getState, dispatch}) {
 //   return (next) => {
 //     return (action) => {
@@ -39,11 +42,12 @@ const store = createStore(
 const homeContainer = document.getElementById("home-container");
 
 render(
-  <BrowserRouter basename="/videos">
+  <BrowserRouter>
     <Provider store={store}>
       <Fragment>
         <Header />
-        <Home />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/videos" component={Videos} />
       </Fragment>
     </Provider>
   </BrowserRouter>,
