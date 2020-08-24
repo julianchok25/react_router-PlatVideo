eec93445809aedd43da95cb99d9107e4e1896bfe
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-23 23:11:48 -0500

    [UPD]notFound: Adding comments with example for Math round and random methods

--- src/pages/components/notFound.js
+++ src/pages/components/notFound.js
@@ -14,6 +14,8 @@ export default function NotFound(props) {
   const handleRandomClick = () => {
     // Generate a random number between 1 and 10
     // 10 is max number and 1 the min, plus 1 is to indicate that starts from 1
+    // Round floor goes from to 0.1 - 0.49 and upper from 0.5
+    // Example: Math.round(0.4 * 9) = 3.6 + 1 = 4.6 = 5
     const random = Math.round(Math.random() * (10 - 1) + 1);
     props.history.push(`/videos?id=${random}`, { id: random });
   };
