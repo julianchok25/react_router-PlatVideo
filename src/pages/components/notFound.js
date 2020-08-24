270afd53010dd6a3b16e14b22646732b03cd7f63
Julian Villegas <julian.villegasplus@gmail.com>
2020-08-23 22:41:48 -0500

    [UPD]notFound: Manipulating the history props that comes from Route and generate a random video with a button redirecting to videos page and use componentDidMount live cicle to run the event openModal

--- src/pages/components/notFound.js
+++ src/pages/components/notFound.js
@@ -2,11 +2,34 @@ import React, { PureComponent } from "react";
 
 import "./generic-page.css";
 
-export default function NotFound() {
+export default function NotFound(props) {
+  const handleBackClick = () => {
+    // props.history.goBack();
+    props.history.go(-1); //This is equivalent to before line
+  };
+  const handleForwardClick = () => {
+    // props.history.goForward();
+    props.history.go(1);
+  };
+  const handleRandomClick = () => {
+    // Generate a random number between 1 and 10
+    // 10 is max number and 1 the min, plus 1 is to indicate that starts from 1
+    const random = Math.round(Math.random() * (10 - 1) + 1);
+    props.history.push(`/videos?id=${random}`, { id: random });
+  };
   return (
     <div className="Page NotFound">
       <h1>404 Not Found..!!</h1>
       <h3 className="SadFace">:(</h3>
+      <button className="Button" onClick={handleForwardClick}>
+        Ir a la siguiente página 👉
+      </button>
+      <button className="Button" onClick={handleBackClick}>
+        Ir a la ruta anterior 👈
+      </button>
+      <button className="Button" onClick={handleRandomClick}>
+        Video Random 🔀
+      </button>
     </div>
   );
 }
